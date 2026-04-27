b29c40b765fc113b9f7bc8482c09904452cea6a7
Junhong Xu <jxuu0216@student.monash.edu>
2026-04-27 23:14:17 +1000

    feat(infra): add CounselingStack and integrate with API Gateway
    - Defined counseling Lambda using AWS CDK
    - Added /counseling-centers route to shared HttpApi
    - Configured environment variables and layer

--- infra/app.py
+++ infra/app.py
@@ -2,6 +2,7 @@ import os
 import aws_cdk as cdk
 from stacks.api_stack import ApiStack
 from stacks.places_stack import PlacesStack
+from stacks.counseling_stack import CounselingStack
 
 app = cdk.App()
 
@@ -22,6 +23,13 @@ api_stack = ApiStack(
 PlacesStack(
     app,
     "PlacesStack",
+    api=api_stack.api,
+    env=env
+)
+
+CounselingStack(
+    app,
+    "CounselingStack",
     api=api_stack.api,
     env=env
 )
